852e3f89fd3f780289252b8cd0ca2157b6006435
Anjan Poonacha <anjanpoonacha369@gmail.com>
2020-02-02 19:28:19 +0530

    Updates

---
+++ precache-manifest.4c3f2b4b91042402148cbb1af63fe1a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42720469d947580ceb77acdd9299b408",
+    "url": "/react-1/index.html"
+  },
+  {
+    "revision": "ab1a4b1e288903a0500a",
+    "url": "/react-1/static/css/main.0d796904.chunk.css"
+  },
+  {
+    "revision": "ae4467ac1b6874332135",
+    "url": "/react-1/static/js/2.f5a471c9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-1/static/js/2.f5a471c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab1a4b1e288903a0500a",
+    "url": "/react-1/static/js/main.4029883f.chunk.js"
+  },
+  {
+    "revision": "4f5a47264d114832a4bb",
+    "url": "/react-1/static/js/runtime-main.6d9c46e8.js"
+  }
+]);
